62892e580c79def780c16432142c9fa5ac96f156
Himanshu <himanshuramchandani08.gmail.com>
2025-07-30 09:44:10 +0530

    initial commit for first 2 phases

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # finance-rag-production
 finance-rag-production
+
+
+## Production Phases
+
+Phase 1: Core RAG Application (Local Python)
+- data, environment, requirements.txt, gitignore
+- app.py
+- rag_pipeline.py
+
+test the app.py using curl:
+curl -X POST -H "Content-Type: application/json" -d '{"question":"What are the main risks?"}' http://localhost:5001/query
+
+
+
+Phase 2: Containerization (Docker)
+package the flask app and chromadb into docker containers
+
+
+
+
+Phase 3: CI/CD Automation (github actions)
+Phase 4: ML Orchestration (Airflow)
+Phase 5: Monitoring & Observability (Grafana and Prometheus)
+Phase 6: Infrastructure as Code (AWS Deployment)
---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+from rag_pipeline import load_and_chunk_pdf, create_vector_store, query_rag
+from prometheus_flask_exporter import PrometheusMetrics
+import os
+
+app = Flask(__name__)
+metrics = PrometheusMetrics(app)
+
+pdf_path = 'data/sample_10k.pdf'
+chunks = load_and_chunk_pdf(pdf_path)
+vector_store = create_vector_store(chunks)
+
+@app.route('/query', methods=['POST'])
+def query():
+    data = request.get_json()
+    question = data.get('question')
+    
+    if not question:
+        return jsonify({"error": "Question is required"}), 400
+    
+    try:
+        answer, sources = query_rag(question, vector_store)
+        return jsonify({
+            "answer": answer,
+            "sources": [doc.page_content for doc in sources]
+        })
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
